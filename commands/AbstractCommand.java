53ffb05dd4b1c5499f6c755ecf03982810435bb5
eroosendahl <eroosendahl@gmail.com>
2022-03-24 23:54:17 -0500

    basic setup and fixing java dependencies

---
+++ commands/AbstractCommand.java
@@ -0,0 +1,12 @@
+package commands;
+
+//base class all further commands will inherit from
+public abstract class AbstractCommand {
+	
+	protected String name;
+	public String getName() { return name; }
+	public abstract int execute();
+	public abstract int execute(String executionMod);
+	public abstract int helpMessage();
+
+}
