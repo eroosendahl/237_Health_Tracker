10d3452e4d6367315cf3a7d656271824dd49116e
clara mckinley <cmckinley@wustl.edu>
2022-03-25 18:11:43 -0500

    added newusercommand and csv file

---
+++ commands/NewUserCommand.java
@@ -0,0 +1,48 @@
+package commands;
+
+
+import commands.AbstractCommand;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NewUserCommand extends AbstractCommand {
+	String csvFileName;
+	
+	public NewUserCommand(String user, String csvFile) {
+		name = user;
+		csvFileName = csvFile;
+	}
+	
+	@Override
+	public int execute() {
+		List<String> user = new LinkedList<String>();
+		user.add("\n" + name);
+		try {
+			File outputFile = new File(csvFileName);
+			FileWriter data = new FileWriter(outputFile, true);
+			String collect = user.stream().collect(Collectors.joining());
+			data.append(collect);
+			data.flush();
+			data.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(name);
+		return 0;
+	}
+	
+	@Override
+	public int execute(String executionMod) {
+		System.out.println(executionMod);
+		return 0;
+	}
+	
+	@Override
+	public int helpMessage() {
+		return 0;
+	}
+}
--- main/HealthTracker.java
+++ main/HealthTracker.java
@@ -1,14 +1,16 @@
 package main;
 
 import commands.EchoCommand;
+import commands.NewUserCommand;
 
 public class HealthTracker {
 	
 	public static void main(String args[]) {
-		
+	
 		EchoCommand echoCommand = new EchoCommand();
-		
+		NewUserCommand newUser = new NewUserCommand("Erik", "userinfo.csv");
 		echoCommand.execute("Hello World");
+		newUser.execute();
 	}
 
 }
