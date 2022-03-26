57f632cc87777e9ab80d7945808be0498cf4b969
fletchni17 <fletcher.n@wustl.edu>
2022-03-25 20:12:56 -0500

    started parse routine for entry input

--- commands/NewEntryCommand.java
+++ commands/NewEntryCommand.java
@@ -4,21 +4,38 @@ public class NewEntryCommand extends AbstractCommand{
 
 	String entryIdentifier;
 	String entryValue;
-	
-	public NewEntryCommand() {
+	String entryDate;
+	User currentUser;
+	public NewEntryCommand(User user) {
 		
+		
+		this.currentUser = User;
 		name = "newEntry";
 	}
 	@Override
 	public int execute() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Value required for insertion.");
 		return 0;
 	}
 
 	@Override
 	public int execute(String executionMod) {
-		// TODO Auto-generated method stub
+		
+		//Ex: "newEntry run 500 09/20/2020"
+		
+		String[] commandSections = executionMod.split(" ");
+		this.entryIdentifier = commandSections[1];
+		this.entryValue = commandSections[2];
+		this.entryDate = commandSections[3];
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		return 0;
 	}
