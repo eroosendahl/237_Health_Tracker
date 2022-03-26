5a7b4dfe1cc4498fa74de9b8003ba3831521dca3
n.tungluntony@wustl.edu <n.tungluntony@wustl.edu>
2022-03-26 12:37:35 -0500

    Add help message

--- commands/EchoCommand.java
+++ commands/EchoCommand.java
@@ -23,6 +23,7 @@ public class EchoCommand extends AbstractCommand {
 	@Override
 	public int helpMessage() {
 		// TODO Auto-generated method stub
+		System.out.println("Format: echo [execution-mode]");
 		return 0;
 	}
 
