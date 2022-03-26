e845bbb5828366b3aea49628edb92341bc31a059
fletchni17 <fletcher.n@wustl.edu>
2022-03-25 20:45:01 -0500

    more progress on parsing routine

--- commands/NewEntryCommand.java
+++ commands/NewEntryCommand.java
@@ -1,16 +1,23 @@
 package commands;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class NewEntryCommand extends AbstractCommand{
 
 	String entryIdentifier;
 	String entryValue;
 	String entryDate;
 	User currentUser;
-	public NewEntryCommand(User user) {
+	String filePath;
+	
+	
+	public NewEntryCommand(User user, String path) {
 		
 		
-		this.currentUser = User;
+		this.currentUser = user;
 		name = "newEntry";
+		this.filePath = path;
 	}
 	@Override
 	public int execute() {
@@ -22,7 +29,10 @@ public class NewEntryCommand extends AbstractCommand{
 	@Override
 	public int execute(String executionMod) {
 		
-		//Ex: "newEntry run 500 09/20/2020"
+		//Ex of command: "newEntry run 500 09/20/2020"
+		
+		//Ex of fileEntry: "03/10/2022 WALK(2) RUN(3) ATE(2200)"
+		
 		
 		String[] commandSections = executionMod.split(" ");
 		this.entryIdentifier = commandSections[1];
@@ -30,11 +40,31 @@ public class NewEntryCommand extends AbstractCommand{
 		this.entryDate = commandSections[3];
 		
 		
+		Scanner sc = new Scanner(new File(this.filePath));  
+		sc.useDelimiter(","); 
 		
+		int count =0;
+		while(count< this.currentUser.getUserRow()) {
+			
+			if(sc.hasNext()) sc.next();
+			
+			else System.out.println("Error finding User position");
+			
+			
+			count++;
+		}
 		
+		String userRow = sc.next();
+		String[] userEntries = userRow.split(",");
 		
-		
-		
+		for(int i =0; i<userEntries.length; i++) {
+			
+			String individualEntry = userEntries[i];
+			
+			String[] entrySections = individualEntry.split(" ");
+			
+			
+		}
 		
 		
 		return 0;
