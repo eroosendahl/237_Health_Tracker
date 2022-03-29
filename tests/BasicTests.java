f4c7f3bc6ec505ea2603f1c8f10388c139a6e8be
fletchni17 <fletcher.n@wustl.edu>
2022-03-29 13:03:28 -0500

    new commit

--- tests/BasicTests.java
+++ tests/BasicTests.java
@@ -1,5 +1,18 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
 public class BasicTests {
+	
+	
+	@Test
+	public void testAddUser() 
+	{
+		
+		NewUserCommand userCom = new NewUserCommand("newUser","test.csv");
+		
+		
+	    
+	}
 
 }
