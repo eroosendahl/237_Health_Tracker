21ef7f9b28be105352e8371e666105f873784ca2
Erik Roosendahl <eroosendahl@gmail.com>
2022-04-26 15:55:38 -0500

    changed up some tests to reflect new function

--- tests/AppendEntryCommandTests.java
+++ tests/AppendEntryCommandTests.java
@@ -1,120 +1,92 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import commands.AppendEntryCommand;
-import commands.DeleteEntryCommand;
-import commands.NewEntryCommand;
-import commands.NewUserCommand;
+import commands.DisplayEntryCommand;
 import main.CommandPrompt;
-import main.User;
+import main.HealthTrackerGeneralVariables;
+import main.HealthTrackerGeneralVariables.endState;
 
 class AppendEntryCommandTests {
 	CommandPrompt commandPrompt;
-	AppendEntryCommand appendEntry;
-	NewUserCommand newUser;
-	NewEntryCommand newEntry;
-	DeleteEntryCommand deleteEntryCommand;
-	String testFileName = "testFile";
+	AppendEntryCommand appendEntryCommand;
+	DisplayEntryCommand displayEntryCommand;
+	String testFileName = "testUserInfo.csv";
+	PrintStream oldOut = System.out;
 
 	@BeforeEach
 	void setup() throws IOException {
 		commandPrompt = new CommandPrompt();
-		createTestFile(testFileName);
+		HealthTrackerGeneralVariables.generateTestFile();
 		commandPrompt.setFile(testFileName);
-		appendEntry = new AppendEntryCommand(commandPrompt);
-		commandPrompt.addCommand(appendEntry);
-		newEntry = new NewEntryCommand(commandPrompt);
-		commandPrompt.addCommand(newEntry);
-		newUser = new NewUserCommand(commandPrompt);
-		commandPrompt.addCommand(newUser);
-		deleteEntryCommand = new DeleteEntryCommand(commandPrompt);
-		commandPrompt.addCommand(deleteEntryCommand);
 		
-		String exampleUserName = "testUser";
-		User originalUser = new User(exampleUserName, 0);
-		String entry1 = "09/01/2001 run 500";
-		String entry2 = "10/01/2001 run 500";
-		
-		newUser.execute(exampleUserName);
-		commandPrompt.setCurrentUser(originalUser);
-		newEntry.execute(entry1);
-		newEntry.execute(entry2);
+		appendEntryCommand = new AppendEntryCommand(commandPrompt);
+		commandPrompt.addCommand(appendEntryCommand);
+		displayEntryCommand = new DisplayEntryCommand(commandPrompt);
+		commandPrompt.addCommand(displayEntryCommand);
 	}
 	
 	@Test
-	void validAppendEntry() throws IOException {
-		String exampleAppendEntry = "09/01/2001 walk 300";
-		appendEntry.execute(exampleAppendEntry);
+	void testAppendToExistingActivity() {
+		String testDate = "01/01/2000";
+		String originalEntry = gatherEntry(testDate);
+		String activity = "run";
+		String value = "1";
 		
-		String newEntry = "09/01/2001 run(500) walk(300)";
-		boolean foundNewEntry = searchForEntry(testFileName, commandPrompt.getCurrentUser(), newEntry);
-		String oldEntry = "10/01/2001 run(500)";
-		boolean foundOldEntry = searchForEntry(testFileName, commandPrompt.getCurrentUser(), oldEntry);
-		deleteTestFile(testFileName);
+		appendEntryCommand.execute(testDate + " " + activity + " " + value);
 		
-		assertTrue(foundNewEntry);
-		assertTrue(foundOldEntry);
-	}
-	
-	@Test
-	void invalidAppendEntry() throws IOException {
-		String exampleAppendEntry = "09/01/2001 walk";
-		appendEntry.execute(exampleAppendEntry);
+		String appendedEntry = gatherEntry(testDate);
 		
-		String newEntry = "09/01/2001 run(500)";
-		boolean foundNewEntry = searchForEntry(testFileName, commandPrompt.getCurrentUser(), newEntry);
-		String oldEntry = "10/01/2001 run(500)";
-		boolean foundOldEntry = searchForEntry(testFileName, commandPrompt.getCurrentUser(), oldEntry);
-		deleteTestFile(testFileName);
+		int originalValue = getActivityValue(originalEntry, activity);
+		int appendedValue = getActivityValue(appendedEntry, activity);
 		
-		assertTrue(foundNewEntry);
-		assertTrue(foundOldEntry);
+		assertEquals(appendedValue-originalValue, Integer.parseInt(value));
 	}
-	
-	public boolean createTestFile(String fileName) throws IOException {
-		File testFile = new File(fileName);
-		return testFile.createNewFile();
-	}
-	
-	public boolean deleteTestFile(String fileName) {
-		File testFile = new File(fileName);
-		return testFile.delete();
-	}
-	
-	public boolean searchForEntry(String fileName, User user ,String entry) throws IOException {
-		File csvFile = new File(fileName);
-		FileReader csvReader = new FileReader(csvFile);
-		BufferedReader csvBufferedReader = new BufferedReader(csvReader);
-		String line = null;
-		boolean found = false;
 
-		int count = 0;
-		while ((line = csvBufferedReader.readLine()) != null) {
-			if(user.getRow()==count) {
-				String[] entries = line.split(",");
-				for(int i =1; i <entries.length; i++) {
-					if(entries[i].contains(entry)) {
-						found = true;
-						csvReader.close();
-						csvBufferedReader.close();
-						return found;
-					}
-				}
-			}
-			count++;
-		}
-		csvReader.close();
-		csvBufferedReader.close();
+	@Test
+	void testAppendToUnexistingActivity() {
+		String testDate = "01/01/2000";
+		String originalEntry = gatherEntry(testDate);
+		String activity = "study";
+		String value = "1";
 		
-		return found;
+		appendEntryCommand.execute(testDate + " " + activity + " " + value);
+		
+		String appendedEntry = gatherEntry(testDate);
+		
+		int originalValue = getActivityValue(originalEntry, activity);
+		int appendedValue = getActivityValue(appendedEntry, activity);
+		
+		assertEquals(originalValue, endState.GENERAL_FAILURE.value());
+		assertEquals(appendedValue, Integer.parseInt(value));
+		
 	}
+	
+	private int getActivityValue(String entry, String activity) {
+		String[] miniEntries = entry.split(" ");
+		for (String str : miniEntries) {
+			if (str.contains(activity)) {
+				return Integer.parseInt(str.substring(str.indexOf("(")+1, str.indexOf(")")));
+			}
+		}
+		return endState.GENERAL_FAILURE.value();
+	}
+
+	private String gatherEntry(String date) {
+		ByteArrayOutputStream newOut = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(newOut));
+		displayEntryCommand.execute(date);
+		System.setOut(oldOut);
+		return newOut.toString();
+	}
+	
+	
 }
