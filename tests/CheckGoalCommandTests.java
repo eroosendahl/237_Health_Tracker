d6331630b6608c6748e480120a253888c915ca8f
Erik Roosendahl <eroosendahl@gmail.com>
2022-04-27 20:26:53 -0500

    cleaning

--- tests/CheckGoalCommandTests.java
+++ tests/CheckGoalCommandTests.java
@@ -15,40 +15,39 @@ import main.CommandPrompt;
 import main.HealthTrackerGeneralVariables;
 
 public class CheckGoalCommandTests {
-	
+
 	@Test
 	void testCheckGoalCommand() {
 		CommandPrompt cp = new CommandPrompt();
 		File testFile = HealthTrackerGeneralVariables.generateTestFile();
 		cp.setFile(testFile.getName());
-		
+
 		SetGoalCommand setGoalCommand = new SetGoalCommand(cp);
 		cp.addCommand(setGoalCommand);
 		SummarizeCommand summary = new SummarizeCommand(cp);
 		cp.addCommand(summary);
-		
+
 		CheckGoalCommand checkGoalCommand = new CheckGoalCommand(cp);
 		cp.addCommand(setGoalCommand);
-		
+
 		String activityName = "run";
 		String goalValue = "5";
 		String startDate = "01/01/2000";
 		String endDate = "03/01/2000";
 		String executionMod = activityName + " " + startDate + " " + endDate;
-		
+
 		setGoalCommand.execute(activityName + " " + goalValue);
-		
+
 		int[] summaryStats = summary.calculateStatistics(executionMod);
 		String total = String.valueOf(summaryStats[1]);
 		String mean = String.valueOf(summaryStats[2]);
-		
+
 		ByteArrayOutputStream newOut = new ByteArrayOutputStream();
 		PrintStream oldOut = System.out;
 		System.setOut(new PrintStream(newOut));
 		checkGoalCommand.execute("run 01/01/2000 03/01/2000");
 		assertTrue(newOut.toString().contains(goalValue));
-		//assertTrue(newOut.toString().contains(total));
-		assertTrue(newOut.toString().contains(mean));	// uncomment if we want to include the mean value in the printed statement from checkGoalCommand
+		assertTrue(newOut.toString().contains(mean));	
 	}
 
 }
